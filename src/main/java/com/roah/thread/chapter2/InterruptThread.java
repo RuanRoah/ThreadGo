0d2a1b794f9b9435fab001020dec911b36197f54
Ruanhang <ruanhang@missfresh.cn>
2018-08-14 17:12:30 +0800

    文件补充

--- src/main/java/com/roah/thread/chapter2/InterruptThread.java
+++ src/main/java/com/roah/thread/chapter2/InterruptThread.java
@@ -12,10 +12,16 @@ public class InterruptThread {
             @Override
             public void run() {
                 while (true) {
+                    if(Thread.currentThread().isInterrupted()){
+                        System.out.println("线程中断，结束");
+                        break;
+                    }
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                        System.out.println("Sleeping!");
+                        Thread.currentThread().interrupt();
                     }
                     System.out.println("Running!");
                     Thread.yield();
